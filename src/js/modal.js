20cfce7c74cd8d8a1e915c475bad04eb61a37db7
HusarOlha <husarolya@gmail.com>
2022-10-19 13:37:20 +0300

    backdrop

--- src/js/modal.js
+++ src/js/modal.js
@@ -3,12 +3,19 @@
     openModalBtn: document.querySelector("[data-modal-open]"),
     closeModalBtn: document.querySelector("[data-modal-close]"),
     modal: document.querySelector("[data-modal]"),
+    body: document.querySelector("body"),
   };
 
   refs.openModalBtn.addEventListener("click", toggleModal);
   refs.closeModalBtn.addEventListener("click", toggleModal);
+  refs.body.addEventListener("click", backdropClose);
 
   function toggleModal() {
     refs.modal.classList.toggle("is-hidden");
+  }
+
+  function backdropClose(event) {
+    const backdrop = document.querySelector("div.backdrop");
+    if(event.target === backdrop) refs.modal.classList.toggle("is-hidden");
   }
 })();
