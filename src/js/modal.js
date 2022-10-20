1d78a47ae4d8999e16cd80988d686fa63edb0e0c
HusarOlha <husarolya@gmail.com>
2022-10-20 12:17:52 +0300

    soud

--- src/js/modal.js
+++ src/js/modal.js
@@ -4,11 +4,15 @@
     closeModalBtn: document.querySelector("[data-modal-close]"),
     modal: document.querySelector("[data-modal]"),
     body: document.querySelector("body"),
+    audio: document.querySelector("audio"),
+    button: document.querySelector("button.about-text__button")
   };
-
+  console.log(refs)
   refs.openModalBtn.addEventListener("click", toggleModal);
   refs.closeModalBtn.addEventListener("click", toggleModal);
   refs.body.addEventListener("click", backdropClose);
+  refs.button.addEventListener("mousemove", sayMou);
+
 
   function toggleModal() {
     refs.modal.classList.toggle("is-hidden");
@@ -17,5 +21,11 @@
   function backdropClose(event) {
     const backdrop = document.querySelector("div.backdrop");
     if(event.target === backdrop) refs.modal.classList.toggle("is-hidden");
+  }
+
+  function sayMou(event) {
+    console.log("EVENT", event)
+    console.log("audio",refs.audio)
+    refs.audio.play();
   }
 })();
