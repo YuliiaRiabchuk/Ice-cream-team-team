2da32f0048e0b995c850eee3313b24b89ad2b3a5
YuliiaRiabchuk <yuliiariabchuk@gmail.com>
2022-10-20 20:31:53 +0300

    cheked bugs

--- src/js/reviews-slider.js
+++ src/js/reviews-slider.js
@@ -1,7 +1,8 @@
-$('.single-item-rtl').slick({
+$('.single-item').slick({
   // autoplaySpeed: 7000,
   // autoplay: true,
   dots: true,
   slidesToShow: 1,
   // slidesToScroll: 1,
+  arrows: false,
 });
